93e130276db1ae18317ffc6fcd8d4d12bc56fc43
Iván Moreno <imoreno.owo@gmail.com>
2023-12-11 17:48:32 +0100

    docs: set goals

--- BabaIsYou/UnitTest1.cs
+++ BabaIsYou/UnitTest1.cs
@@ -1,12 +1,20 @@
 namespace BabaIsYou;
 
+//OBJETIVO INICIAL
+//1. Algo se identifica como TÚ
+//2. TÚ te puedes mover
+
+//PARA ALCANZARLO
+//1. TÚ puedes ser cualquier cosa
+//2. Se usan frases para definir qué eres TÚ
+    //2.1. Las frases siempre van unidas por un "is"
+    //2.2. Las frases empiezan por un sujeto
+    //2.3. Las frases pueden acabar con un adjetivo u otro sujeto
+//3. Una frase puede tener o no sentido.
+    //3.1 Cuando una frase tiene sentido resulta en un cambio de estado para el sujeto
+
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
     public void Test1()
     {
