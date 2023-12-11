068757445967abd6059ffabd80e88ed594f107ee
Iván Moreno <imoreno.owo@gmail.com>
2023-12-11 17:59:37 +0100

    feat: attach no definitions when there are no blocks

--- BabaIsYou/UnitTest1.cs
+++ BabaIsYou/UnitTest1.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace BabaIsYou;
 
 //OBJETIVO INICIAL
@@ -7,17 +9,27 @@ namespace BabaIsYou;
 //PARA ALCANZARLO
 //1. TÚ puedes ser cualquier cosa
 //2. Se usan frases para definir qué eres TÚ
-    //2.1. Las frases siempre van unidas por un "is"
-    //2.2. Las frases empiezan por un sujeto
-    //2.3. Las frases pueden acabar con un adjetivo u otro sujeto
+//2.1. Las frases siempre van unidas por un "is"
+//2.2. Las frases empiezan por un sujeto
+//2.3. Las frases pueden acabar con un adjetivo u otro sujeto
 //3. Una frase puede tener o no sentido.
-    //3.1 Cuando una frase tiene sentido resulta en un cambio de estado para el sujeto
+//3.1 Cuando una frase tiene sentido resulta en un cambio de estado para el sujeto
 
 public class Tests
 {
     [Test]
-    public void Test1()
+    public void NothingDefinesSubject_WhenNoBlockExist()
     {
-        Assert.Pass();
+        Array.Empty<((int x, int y), string subject)>()
+            .DefinitionOf("Baba")
+            .Should().BeEmpty();
+    }
+}
+
+public static class safsafsa
+{
+    public static string DefinitionOf(this IEnumerable<((int x, int y), string block)> blocks, string subject)
+    {
+        return string.Empty;
     }
 }
