e741765fe572be29019da83ea52113037caf779e
Iván Moreno <imoreno.owo@gmail.com>
2023-12-12 17:22:55 +0100

    refactor: delete subject notion

--- BabaIsYou/Subject.cs
+++
@@ -1,9 +0,0 @@
-namespace BabaIsYou;
-
-public readonly struct Subject
-{
-    readonly string whatIs;
-    Subject(string whatIs) => this.whatIs = whatIs;
-    public static Subject Baba => new("Baba");
-    public static implicit operator string(Subject subject) => subject.whatIs;
-}
