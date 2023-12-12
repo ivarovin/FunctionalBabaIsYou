07d0459a9e70c8219d0574616f86c8fc5f67b86a
Iván Moreno <imoreno.owo@gmail.com>
2023-12-12 17:05:31 +0100

    refactor: extract grammar class

---
+++ BabaIsYou/Grammar.cs
@@ -0,0 +1,21 @@
+namespace BabaIsYou;
+
+public static class Grammar
+{
+    public static string DefinitionOf(this IEnumerable<((int x, int y), string block)> blocks, Subject what)
+    {
+        if (!blocks.Any(x => x.block == what))
+            throw new ArgumentException($"Subject {what} is not defined");
+        if (!blocks.Any())
+            return string.Empty;
+        if (!blocks.Any(x => x.block == "is"))
+            return string.Empty;
+        
+        var linkingVerbPosition = blocks.First(x => x.block == "is").Item1;
+        var subjectPosition = blocks.First(x => x.block == what).Item1;
+        if (subjectPosition.x > linkingVerbPosition.x)
+            return string.Empty;
+
+        return blocks.Last().block;
+    }
+}
--- BabaIsYou/UnitTest1.cs
+++ BabaIsYou/UnitTest1.cs
@@ -50,23 +50,3 @@ public class Tests
             .Should().BeEmpty();
     }
 }
-
-public static class safsafsa
-{
-    public static string DefinitionOf(this IEnumerable<((int x, int y), string block)> blocks, Subject what)
-    {
-        if (!blocks.Any(x => x.block == what))
-            throw new ArgumentException($"Subject {what} is not defined");
-        if (!blocks.Any())
-            return string.Empty;
-        if (!blocks.Any(x => x.block == "is"))
-            return string.Empty;
-        
-        var linkingVerbPosition = blocks.First(x => x.block == "is").Item1;
-        var subjectPosition = blocks.First(x => x.block == what).Item1;
-        if (subjectPosition.x > linkingVerbPosition.x)
-            return string.Empty;
-
-        return blocks.Last().block;
-    }
-}
