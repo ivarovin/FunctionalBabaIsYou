26c709ae08aec2beb7edbc03e0485733f7129bbd
Iván Moreno <imoreno.owo@gmail.com>
2023-12-12 17:52:40 +0100

    refactor: encapsulate all guard clauses

--- BabaIsYou/Grammar.cs
+++ BabaIsYou/Grammar.cs
@@ -2,23 +2,23 @@ namespace BabaIsYou;
 
 public static class Grammar
 {
-    public static string DefinitionOf(this IEnumerable<((int x, int y), string block)> blocks, string what)
+    public static string DefinitionOf(this IEnumerable<((int x, int y) whereIs, string block)> blocks, string what)
     {
         if (!blocks.Any(x => x.block == what))
             throw new ArgumentException($"Subject {what} is not defined");
-        
-        if (!blocks.Any())
-            return string.Empty;
-        if (!blocks.ExistsLinkingVerbFor(what))
-            return string.Empty;
-        if (!blocks.Any(x => x.Item1.x > blocks.WhereIsLinkingVerbFor(what).x))
-            return string.Empty;
-        
-        var definition = blocks.First(x => x.Item1.x > blocks.WhereIsLinkingVerbFor(what).x);
-        return definition.block;
+
+        return blocks.ContainsDefinitionFor(what)
+            ? blocks.First(block => block.whereIs.x > blocks.WhereIsLinkingVerbFor(what).x).block
+            : string.Empty;
     }
 
-    static (int x, int y) WhereIsLinkingVerbFor(this IEnumerable<((int x, int y), string block)> blocks, string what) 
+    static bool ContainsDefinitionFor(this IEnumerable<((int x, int y), string block)> blocks, string what)
+        => blocks.Any() && blocks.ExistsLinkingVerbFor(what) && blocks.ExistsDefinitionFor(what);
+
+    static bool ExistsDefinitionFor(this IEnumerable<((int x, int y), string block)> blocks, string what)
+        => blocks.Any(x => x.Item1.x > blocks.WhereIsLinkingVerbFor(what).x);
+
+    static (int x, int y) WhereIsLinkingVerbFor(this IEnumerable<((int x, int y), string block)> blocks, string what)
         => blocks.First(x => x.block == "is" && x.Item1.x > blocks.WhereIs(what).x).Item1;
 
     static (int x, int y) WhereIs(this IEnumerable<((int x, int y), string block)> blocks, string what)
