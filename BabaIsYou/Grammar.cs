937b0f6f76198d9552da2528175f3a21a65525eb
Iván Moreno <imoreno.owo@gmail.com>
2023-12-12 18:15:14 +0100

    refactor: check what is at the right of block

--- BabaIsYou/Grammar.cs
+++ BabaIsYou/Grammar.cs
@@ -8,28 +8,28 @@ public static class Grammar
             throw new ArgumentException($"Subject {subject} is not defined");
 
         return blocks.ContainsDefinitionFor(subject)
-            ? blocks.First(block =>
-                block.whereIs.x > blocks.WhereIsLinkingVerbFor(subject).x &&
-                block.AtSameHeightThan(blocks.WhereIs(subject))).block
+            ? blocks.First(block => block.IsToTheRightOf(blocks.WhereIsLinkingVerbFor(subject))).block
             : string.Empty;
     }
 
     static bool ContainsDefinitionFor(this IEnumerable<((int x, int y), string block)> blocks, string what)
         => blocks.Any() && blocks.ExistsLinkingVerbFor(what) && blocks.ExistsDefinitionFor(what);
 
-    static bool ExistsDefinitionFor(this IEnumerable<((int x, int y), string block)> blocks, string what)
-        => blocks.Any(x => x.Item1.x > blocks.WhereIsLinkingVerbFor(what).x &&
-                           x.AtSameHeightThan(blocks.WhereIsLinkingVerbFor(what)));
+    static bool ExistsDefinitionFor(this IEnumerable<((int x, int y), string block)> blocks, string subject)
+        => blocks.Any(block => block.IsToTheRightOf(blocks.WhereIsLinkingVerbFor(subject)));
 
-    static (int x, int y) WhereIsLinkingVerbFor(this IEnumerable<((int x, int y), string block)> blocks, string what)
-        => blocks.First(x => x.block == "is" && x.Item1.x > blocks.WhereIs(what).x).Item1;
+    static bool IsToTheRightOf(this ((int x, int y) whereIs, string block) block, (int x, int y) where)
+        => block.whereIs.x > where.x && block.AtSameHeightThan(where);
+
+    static (int x, int y) WhereIsLinkingVerbFor(this IEnumerable<((int x, int y) whereIs, string block)> blocks,
+        string what)
+        => blocks.First(x => x.block == "is" && x.IsToTheRightOf(blocks.WhereIs(what))).whereIs;
 
     static (int x, int y) WhereIs(this IEnumerable<((int x, int y), string what)> blocks, string what)
         => blocks.First(x => x.what == what).Item1;
 
     static bool ExistsLinkingVerbFor(this IEnumerable<((int x, int y), string what)> blocks, string subject)
-        => blocks.Any(block => block.what == "is" && block.Item1.x > blocks.WhereIs(subject).x &&
-                               block.AtSameHeightThan(blocks.WhereIs(subject)));
+        => blocks.Any(block => block.what == "is" && block.IsToTheRightOf(blocks.WhereIs(subject)));
 
     static bool AtSameHeightThan(this ((int x, int y) whereIs, string what) block, (int x, int y) subject)
         => block.whereIs.y == subject.y;
