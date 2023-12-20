1326cdde5b1998b2e9f0a053ba3b20211804bc4e
Iván Moreno <imoreno.owo@gmail.com>
2023-12-20 17:35:43 +0100

    refactor: order methods by call order

--- BabaIsYou/DefinitionSearch.cs
+++ BabaIsYou/DefinitionSearch.cs
@@ -7,19 +7,18 @@ public class DefinitionSearch
     readonly IEnumerable<((int x, int y), string block)> blocks;
     readonly string subject;
 
-    (int x, int y) WhereIsSubject => blocks.WhereIs(subject);
-    Option<((int x, int y), string block)> LinkingVerb => blocks.Where(IsLinkingVerb).FirstOrNone(AtRightOfSubject);
-    Option<(int x, int y)> WhereIsDefinition => LinkingVerb.Map(ToTheRight);
     public Option<((int x, int y), string block)> Definition => WhereIsDefinition.Bind(Block);
+    Option<(int x, int y)> WhereIsDefinition => LinkingVerb.Map(ToTheRight);
+    Option<((int x, int y), string block)> LinkingVerb => blocks.Where(IsLinkingVerb).FirstOrNone(AtRightOfSubject);
+    static bool IsLinkingVerb(((int x, int y), string block) x) => x.block == "is";
+    bool AtRightOfSubject(((int x, int y), string block) block) => block.Item1.x == WhereIsSubject.x + 1 && block.Item1.y == WhereIsSubject.y;
+    (int x, int y) WhereIsSubject => blocks.WhereIs(subject);
+    static (int, int y) ToTheRight(((int x, int y), string block) x) => (x.Item1.x + 1, x.Item1.y);
+    Option<((int x, int y), string)> Block((int x, int y) at) => blocks.FirstOrNone(x => x.Item1 == at);
 
     public DefinitionSearch(IEnumerable<((int x, int y), string block)> blocks, string subject)
     {
         this.blocks = blocks;
         this.subject = subject;
     }
-
-    bool AtRightOfSubject(((int x, int y), string block) block) => block.Item1.x == WhereIsSubject.x + 1 && block.Item1.y == WhereIsSubject.y;
-    static bool IsLinkingVerb(((int x, int y), string block) x) => x.block == "is";
-    Option<((int x, int y), string)> Block((int x, int y) at) => blocks.FirstOrNone(x => x.Item1 == at);
-    static (int, int y) ToTheRight(((int x, int y), string block) x) => (x.Item1.x + 1, x.Item1.y);
 }
--- BabaIsYou/SubjectSearch.cs
+++ BabaIsYou/SubjectSearch.cs
@@ -7,20 +7,18 @@ public class SubjectSearch
     readonly IEnumerable<((int x, int y), string block)> blocks;
     readonly string definition;
 
-    (int x, int y) WhereIsDefinition => blocks.WhereIs(definition);
-    Option<((int x, int y), string block)> LinkingVerb => blocks.Where(IsLinkingVerb).FirstOrNone(AtLeftOfDefinition);
-    Option<(int x, int y)> WhereIsSubject => LinkingVerb.Map(ToTheLeft);
     public Option<((int x, int y), string block)> Subject => WhereIsSubject.Bind(Block);
+    Option<(int x, int y)> WhereIsSubject => LinkingVerb.Map(ToTheLeft);
+    Option<((int x, int y), string block)> LinkingVerb => blocks.Where(IsLinkingVerb).FirstOrNone(AtLeftOfDefinition);
+    static bool IsLinkingVerb(((int x, int y), string block) x) => x.block == "is";
+    bool AtLeftOfDefinition(((int x, int y), string block) block) => block.Item1.x == WhereIsDefinition.x - 1 && block.Item1.y == WhereIsDefinition.y;
+    (int x, int y) WhereIsDefinition => blocks.WhereIs(definition);
+    static (int, int y) ToTheLeft(((int x, int y), string block) x) => (x.Item1.x - 1, x.Item1.y);
+    Option<((int x, int y), string)> Block((int x, int y) at) => blocks.FirstOrNone(x => x.Item1 == at);
 
     public SubjectSearch(IEnumerable<((int x, int y), string block)> blocks, string definition)
     {
         this.blocks = blocks;
         this.definition = definition;
     }
-
-    bool AtLeftOfDefinition(((int x, int y), string block) block) => block.Item1.x == WhereIsDefinition.x - 1 && block.Item1.y == WhereIsDefinition.y;
-    static bool IsLinkingVerb(((int x, int y), string block) x) => x.block == "is";
-    Option<((int x, int y), string)> Block((int x, int y) at) => blocks.FirstOrNone(x => x.Item1 == at);
-    static (int, int y) ToTheLeft(((int x, int y), string block) x) => (x.Item1.x - 1, x.Item1.y);
-    
 }
