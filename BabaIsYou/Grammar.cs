ccd46524e06491cf4724a79c78b404bcc1b67c89
Iván Moreno <imoreno.owo@gmail.com>
2023-12-20 17:12:08 +0100

    refactor: remove dead grammar code

--- BabaIsYou/Grammar.cs
+++ BabaIsYou/Grammar.cs
@@ -19,22 +19,14 @@ public static class Grammar
         if (!blocks.Any(x => x.block == subject))
             throw new ArgumentException($"Subject {subject} is not defined");
 
-        return new DefinitionSearch(blocks, subject)
-            .Definition
-            .Match(x => x.block, () => string.Empty);
+        return new DefinitionSearch(blocks, subject).Definition.Match(x => x.block, () => string.Empty);
     }
-    
+
     public static Option<T> FirstOrNone<T>(this IEnumerable<T> enumerable, Func<T, bool> predicate)
         => enumerable.Any(predicate) ? enumerable.First(predicate) : Option<T>.None;
 
     static bool ContainsSubjectFor(this IEnumerable<((int x, int y) whereIs, string block)> blocks, string definition)
         => blocks.Any() && blocks.ExistsLinkingVerbForDefinition(definition) && blocks.ExistsSubjectFor(definition);
-
-    static bool ContainsDefinitionFor(this IEnumerable<((int x, int y), string)> blocks, string what)
-        => blocks.Any() && blocks.ExistsLinkingVerbForSubject(what) && blocks.ExistsDefinitionFor(what);
-
-    static bool ExistsDefinitionFor(this IEnumerable<((int x, int y), string)> blocks, string subject)
-        => blocks.Any(block => block.IsToTheRightOf(blocks.WhereIsLinkingVerbForSubject(subject)));
 
     static bool ExistsSubjectFor(this IEnumerable<((int x, int y), string)> blocks, string definition)
         => blocks.Any(block => block.IsToTheLeftOf(blocks.WhereIsLinkingVerbForDefinition(definition)));
@@ -45,10 +37,6 @@ public static class Grammar
     static bool IsToTheLeftOf(this ((int x, int y) whereIs, string block) block, (int x, int y) where)
         => block.whereIs.x == where.x - 1 && block.AtSameHeightThan(where);
 
-    static (int x, int y) WhereIsLinkingVerbForSubject(this IEnumerable<((int x, int y) whereIs, string block)> blocks,
-        string what)
-        => blocks.First(x => x.block == "is" && x.IsToTheRightOf(blocks.WhereIs(what))).whereIs;
-
     static (int x, int y) WhereIsLinkingVerbForDefinition(
         this IEnumerable<((int x, int y) whereIs, string block)> blocks,
         string what)
@@ -56,9 +44,6 @@ public static class Grammar
 
     public static (int x, int y) WhereIs(this IEnumerable<((int x, int y), string what)> blocks, string what)
         => blocks.First(x => x.what == what).Item1;
-
-    static bool ExistsLinkingVerbForSubject(this IEnumerable<((int x, int y), string what)> blocks, string subject)
-        => blocks.Any(block => block.what == "is" && block.IsToTheRightOf(blocks.WhereIs(subject)));
 
     static bool ExistsLinkingVerbForDefinition(this IEnumerable<((int x, int y), string what)> blocks,
         string definition)
