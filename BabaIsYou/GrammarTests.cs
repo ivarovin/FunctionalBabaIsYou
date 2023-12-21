3ff968f6303b1d3a79f1fb8640fde7c589a1fc50
Iván Moreno <imoreno.owo@gmail.com>
2023-12-21 18:52:42 +0100

    feat: move towards direction if you are defined

--- BabaIsYou/GrammarTests.cs
+++ BabaIsYou/GrammarTests.cs
@@ -180,7 +180,8 @@ public class GrammarTests
     [Test]
     public void Move_TowardsDirection_IfYou_AreDefined()
     {
-        World.CreateWith(new[] { Baba.AtOrigin() }, new[] { Baba.AtOrigin(), ToBe.AtMiddle(), You.AtRight() }.MoveToRight(1))
+        World.CreateWith(new[] { Baba.AtOrigin() },
+                new[] { Baba.AtOrigin(), ToBe.AtMiddle(), You.AtRight() }.MoveToRight(1))
             .MoveTowards((0, 1))
             .WhereIs(Baba)
             .Should().NotBe((0, 0));
@@ -189,24 +190,45 @@ public class GrammarTests
 
 public record World
 {
+    readonly IEnumerable<((int x, int y), string what)> blocks;
+    readonly IEnumerable<((int x, int y), string what)> actors;
+
+    World(IEnumerable<((int x, int y), string what)> actors, IEnumerable<((int x, int y), string what)> blocks)
+    {
+        this.blocks = blocks;
+        this.actors = actors;
+    }
+
     public static World CreateWith(((int x, int y), string what)[] actors)
     {
-        return new();
+        return new(actors, Array.Empty<((int x, int y), string what)>());
     }
 
     public static World CreateWith(((int x, int y), string what)[] actors,
         IEnumerable<((int x, int y), string what)> blocks)
     {
-        return new();
+        return new(actors, blocks);
     }
 
     public (int x, int y) WhereIs(string baba)
     {
-        return (0, 0);
+        if (!actors.Any(IsYou))
+            return (0, 0);
+        
+        return actors.Where(IsYou).First().Item1;
     }
 
-    public World MoveTowards((int, int) valueTuple)
+    public World MoveTowards((int, int) direction)
     {
-        return new();
+        var you = actors.Where(IsYou);
+        var otherThanYou = actors.Except(you);
+        var newActors = otherThanYou.Concat(you.Select(Move(direction)));
+
+        return new World(newActors, blocks);
     }
+
+    Func<((int x, int y) whereIs, string what), ((int x, int y) whereIs, string what)> Move((int x, int y) direction)
+        => block => ((block.whereIs.x + direction.x, block.whereIs.y + direction.y), block.what);
+
+    bool IsYou(((int x, int y), string whatIs) actor) => blocks.DefinitionOf(actor.whatIs).Equals(You);
 }
