303a219a72c4d02335d55cbc4b60ec2190101a0e
Iván Moreno <imoreno.owo@gmail.com>
2023-12-22 20:37:39 +0100

    refactor: remove dead code

--- BabaIsYou/Grammar.cs
+++ BabaIsYou/Grammar.cs
@@ -1,5 +1,3 @@
-using FunctionalBabaIsYou.Tests;
-
 namespace FunctionalBabaIsYou;
 
 public static class Grammar
@@ -12,17 +10,6 @@ public static class Grammar
                 () => string.Empty
             );
 
-    public static string DefinitionOf(this IEnumerable<((int x, int y) whereIs, string block)> blocks, string subject)
-        => new DefinitionSearch(blocks.ToPlacedBlocks(), subject)
-            .Definition.Match
-            (
-                x => x.whatDepicts,
-                () => string.Empty
-            );
-    
-    public static string DefinitionOf(this IEnumerable<(Coordinate whereIs, string block)> blocks, string subject)
-        => blocks.ToTuples().DefinitionOf(subject);
-    
     public static string DefinitionOf(this IEnumerable<PlacedBlock> blocks, string subject)
         => new DefinitionSearch(blocks, subject)
             .Definition.Match
--- BabaIsYou/WorldBuilder.cs
+++ BabaIsYou/WorldBuilder.cs
@@ -12,6 +12,12 @@ public class WorldBuilder
         this.blocks = blocks;
         return this;
     }
+    
+    public WorldBuilder AndBlocks(params PlacedBlock[] blocks)
+    {
+        this.blocks = blocks;
+        return this;
+    }
 
     public static WorldBuilder IntroduceToWorld(params PlacedBlock[] actors) => new() { actors = actors };
     public static WorldBuilder EmptyWorldWithBlocks(params PlacedBlock[] blocks) => new() { blocks = blocks };
