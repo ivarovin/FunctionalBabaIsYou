d89f35c7c84a7e61ea76f0b0d8406ad98d3e85c1
Iván Moreno <imoreno.owo@gmail.com>
2023-12-22 19:40:51 +0100

    refactor: introduce placed block notion

---
+++ BabaIsYou/PlacedBlock.cs
@@ -0,0 +1,18 @@
+using FunctionalBabaIsYou.Tests;
+
+namespace FunctionalBabaIsYou;
+
+public readonly struct PlacedBlock
+{
+    public readonly Coordinate whereIs;
+    public readonly string whatDepicts;
+
+    public PlacedBlock(Coordinate whereIs, string whatDepicts)
+    {
+        this.whereIs = whereIs;
+        this.whatDepicts = whatDepicts;
+    }
+    
+    public static implicit operator (Coordinate whereIs, string whatDepicts) (PlacedBlock placedBlock) => (placedBlock.whereIs, placedBlock.whatDepicts);
+    public static implicit operator PlacedBlock ((Coordinate whereIs, string whatDepicts) block) => new(block.whereIs, block.whatDepicts);
+}
