3dd543cf501bd0b851ae61df411c58edbb20118e
Iván Moreno <imoreno.owo@gmail.com>
2023-12-28 19:23:57 +0100

    test: push two blocks

--- BabaIsYou/WorldPhysicsTests.cs
+++ BabaIsYou/WorldPhysicsTests.cs
@@ -27,4 +27,18 @@ public class WorldPhysicsTests
             .MoveTowards(Direction.Right)
             .ElementsAt(Middle).Should().HaveCount(2);
     }
+
+    [Test]
+    public void PushTwoBlocks()
+    {
+        var sut = IntroduceToWorld(Baba.AtOrigin(), Rock.AtMiddle(), Rock.AtRight())
+            .AndBlocks(BabaIsYou.Up())
+            .AndBlocks(RockIsPush.Down())
+            .Build()
+            .MoveTowards(Direction.Right);
+
+        sut.ElementsAt((1, 0)).Should().HaveCount(1).And.Contain(Baba.At(1, 0));
+        sut.ElementsAt((2, 0)).Should().HaveCount(1).And.Contain(Rock.At(2, 0));
+        sut.ElementsAt((3, 0)).Should().HaveCount(1).And.Contain(Rock.At(3, 0));
+    }
 }
