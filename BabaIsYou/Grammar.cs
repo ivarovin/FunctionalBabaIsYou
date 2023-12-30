6de1fba7f1e5aa36f922a50406e4d8a35377471a
Iván Moreno <imoreno.owo@gmail.com>
2023-12-30 11:47:46 +0100

    refactor: remove unused subject search feature

--- BabaIsYou/Grammar.cs
+++ BabaIsYou/Grammar.cs
@@ -4,14 +4,6 @@ namespace FunctionalBabaIsYou;
 
 public static class Grammar
 {
-    public static string WhatIs(this IEnumerable<PlacedBlock> blocks, string definition)
-        => new SubjectSearch(blocks, definition)
-            .Subject.Match
-            (
-                x => x.whatDepicts,
-                () => string.Empty
-            );
-
     public static string DefinitionOf(this IEnumerable<PlacedBlock> blocks, string subject)
         => new DefinitionSearch(blocks, subject.AtRight())
             .Definition.Match
