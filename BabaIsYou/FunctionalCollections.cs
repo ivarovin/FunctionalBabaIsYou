7f70db8dc58c52fa7936ad325ba6a177fc27dddd
Iván Moreno <imoreno.owo@gmail.com>
2023-12-30 11:51:06 +0100

    refactor: merge functional collections into monadic query

--- BabaIsYou/FunctionalCollections.cs
+++
@@ -1,7 +0,0 @@
-namespace FunctionalBabaIsYou.Tests;
-
-public static class FunctionalCollections
-{
-    public static T LastOr<T>(this IEnumerable<T> elements, T alternative) 
-        => elements.Any() ? elements.Last() : alternative;
-}
--- BabaIsYou/MonadicQuery.cs
+++ BabaIsYou/MonadicQuery.cs
@@ -6,4 +6,7 @@ public static class MonadicQuery
 {
     public static Option<T> FirstOrNone<T>(this IEnumerable<T> enumerable, Func<T, bool> predicate)
         => enumerable.Any(predicate) ? enumerable.First(predicate) : Option<T>.None;
+    
+    public static T LastOr<T>(this IEnumerable<T> elements, T alternative) 
+        => elements.Any() ? elements.Last() : alternative;
 }
